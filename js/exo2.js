5a7d4afef422f399fa160e3e9307844e313ff046
Ivan Veltz <ivan.veltz@live.fr>
2025-03-21 11:48:37 +0100

    Exo 1 et 2

---
+++ js/exo2.js
@@ -0,0 +1,13 @@
+// querySelectoAll recupère dans un tableau tous les élément avec la class square
+const squares = document.querySelectorAll('.square');
+const bottomSquare = document.querySelector('.bottom-square');
+
+// Rajoute un événement au click sur chaque carré
+squares.forEach(square => {
+    square.addEventListener('click', () => {
+        const color = window.getComputedStyle(square).backgroundColor;
+        // On récupère la couleur de fond du carré
+        bottomSquare.style.backgroundColor = color;
+        // On change la couleur du carré du bas
+    })
+})
