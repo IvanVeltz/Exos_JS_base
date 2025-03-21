5beffb364847948ba1f22c460e0c4b4ea4fb50de
Ivan Veltz <ivan.veltz@live.fr>
2025-03-21 13:39:41 +0100

    Exo3 ok

--- js/exo3.js
+++ js/exo3.js
@@ -2,12 +2,15 @@ const squares = document.querySelectorAll('.square');
 const square = document.querySelector('.square');
 const green = window.getComputedStyle(square).backgroundColor;
 
-squares.forEach(squares => {
-    squares.addEventListener('click', () => {
+squares.forEach(square => {
+    square.addEventListener('click', () => {
         if (window.getComputedStyle(square).backgroundColor === green){
-
+            square.style.backgroundColor = 'rgb(170,113,108)';
+            square.style.transform = "rotate(360deg) scale(0.9)";
+            square.style.transition = "4s";
         } else {
-            
+            square.style.backgroundColor = green;
+            square.style.transform = "rotate(0deg) scale(1)";
         }
     })
 })
