1f92f5a4655c97a9b8453ecb6ce5500eca030c77
Ivan Veltz <ivan.veltz@live.fr>
2025-03-24 14:09:15 +0100

    FIN

--- js/quote.js
+++ js/quote.js
@@ -1,6 +1,6 @@
 var quotes = {
     1: { author: "Serge Karamazov", quote: "Barrez vous **** de mimes." },
-    2: { author: "Serge Karamazov", quote: "Barrez vous **** de mimes Test." },
+    2: { author: "Serge Karamazov", quote: "Non non, je l'ai perdue, c'est-à-dire que je l'ai perdue quoi, elle était là et poupouf, je l'ai perdue…" },
     3: { author: "Geroges Abitbol", quote: "Le train de tes injures roule sur le rail de mon indifférence." },
     4: { author: "Hubert Bonnisseur de la Bath", quote: "Ca ne prenait pas beaucoup plus de temps" },
     5: { author: "Moulinier", quote: "L'habit ne fait pas le moine, mais il fait l'agnet... Même non titularisé" },
